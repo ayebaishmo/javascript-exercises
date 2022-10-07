29deb387ac1cd972a634887f051e591ab2e0dfff
ayebaishmo <ishmo255super@gmail.com>
2022-10-07 16:30:52 +0300

    Passed all 4 test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+ var reversedString = "";
+ for(var i = str.length-1; i >= 0; i--) {
+  reversedString = reversedString + str[i]
+ }
+ return(reversedString)
 };
 
 // Do not edit below this line
