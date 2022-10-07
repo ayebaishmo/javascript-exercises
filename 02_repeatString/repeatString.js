7ae853fb8463fa2b0925e4813e7e67b0f7a0d7ef
ayebaishmo <ishmo255super@gmail.com>
2022-10-07 16:12:45 +0300

    Pass all 7 test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+ var strArray = "";
+ if (num < 0){
+   return('ERROR')
+  } else {
+  for(var i = 1; i<=num; i++) {
+   strArray = strArray + string;
+  }
+ }
+ return(strArray);
 };
 
+repeatString('hey', -1);
 // Do not edit below this line
 module.exports = repeatString;
