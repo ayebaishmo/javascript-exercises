2e10a0bd074654b920102a6643d5f237fcf713e2
ayebaishmo <ishmo255super@gmail.com>
2022-10-16 15:50:22 +0300

    Passed all the test for Sumall

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+ if (!Number.isInteger(x) || !Number.isInteger(y)) {
+  return('ERROR');
+ }
+ if (x < 0 || y < 0) {
+  return('ERROR');
+ }
+ if (x > y) {
+  const temp = x;
+  x = y;
+  y = temp;
+ }
+ const range = [];
+ let sum = 0;
+ for(var i = x; i<=y; i++) {
+  range.push(i);
+ }
+ for(var p = 0; p<range.length; p++) {
+  sum =sum + range[p];
+ }
+ return(sum);
 };
-
+sumAll(1, 4);
 // Do not edit below this line
 module.exports = sumAll;
