0e56c962843dab9a587a7a6f9542477f74a5b60b
ayebaishmo <ishmo255super@gmail.com>
2022-10-17 12:37:43 +0300

    Passed all test for temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+  const result = Math.round((far - 32) * (5/9) * 10)  / 10;
+  return(result);
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  const cresult =(cel * 1.8)  + 32;
+  return(cresult);
 };
 
+ftoc(100);
+ctof(0);
 // Do not edit below this line
 module.exports = {
   ftoc,
