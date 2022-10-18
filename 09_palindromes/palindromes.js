06ca3e04b9507f102a6a6ea6535865214785c76c
ayebaishmo <ishmo255super@gmail.com>
2022-10-18 16:22:38 +0300

    Passed all test for paliindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+ var convertString = str.toLowerCase().replace(/[^a-z]/g, "");
+ var reverseString = convertString.split('').reverse().join('')
+ if(convertString === reverseString) {
+  return true;
+ } else {
+  return false; 
+ }
 };
 
 // Do not edit below this line
